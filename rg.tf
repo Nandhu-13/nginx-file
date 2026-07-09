72e3c9833c3818557241e08106bcf5888cae7e58
NandaKishore <parnatinanda@gmail.com>
2026-07-09 11:34:07 +0530

    m1

--- rg.tf
+++ rg.tf
@@ -1,3 +1,4 @@
-resource "resource_group" "name" {
-  
+resource "azurerm_resource_group" "example" {
+  name     = "rg"
+  location = "West Europe"
 }
--- vnet.tf
+++ vnet.tf
@@ -1,3 +1,5 @@
-provider "sjg"{
-
+resource "azurerm_virtual_network" "vnet" {
+  name = "vnet1"
+  resource_group_name = azurerm_resource_group.example.name
+  location = azurerm_resource_group.example.location
 }
